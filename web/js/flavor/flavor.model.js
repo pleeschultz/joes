74272f585efdcf624eece59622269d7e3f89122d
Peter Schultz <pleeschultz@gmail.com>
2014-07-14 20:38:53 -0500

    images

--- web/js/flavor/flavor.model.js
+++ web/js/flavor/flavor.model.js
@@ -12,7 +12,19 @@ function(
 			kosher: true
 		},
 		initialize: function(attributes, options) {
-		//	console.log('flavor', this);
+			//console.log('flavor', this);
+
+			var type = '-' + this.get('type');
+			var imageId = this.get('id').split(type).join('');
+
+			if(!this.get('hasNameplate')) imageId = 'no-nameplate';
+
+			var image = 'images/nameplates/' + imageId + '.jpg';
+
+
+
+			this.set({ image: image });
+
 		}
 	});
 
