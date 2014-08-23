f3eef0184d0fe6886ec4f77a9c1131eb9057a410
Peter Schultz <pleeschultz@gmail.com>
2014-08-23 12:09:19 -0500

    gulp deplpoyment to mt

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gzip = require('gulp-gzip');
 var clean = require('gulp-clean');
 var uglify = require('gulp-uglify');
 var exec = require('gulp-exec');
+var rimraf = require('gulp-rimraf');
 
 gulp.task('default', ['sass'], function() {
 	gulp.watch('./web/sass/**', ['sass']);
@@ -22,13 +23,14 @@ gulp.task('sass', function () {
 /* clean build */
 gulp.task('build-reset', ['sass'], function(){
 	return gulp.src('./build', { read: false })
-		.pipe(clean({ force: true }));
+		.pipe(rimraf());
 });
 
 gulp.task('build-copy', ['build-reset'], function(){
 	return gulp.src(['web/**', '!./web/sass{,/**}', '!./web/js{,/**}'])
 		.pipe(gulp.dest('./build/web'));
 });
+
 
 gulp.task('require-js-build', ['build-copy'], function(){
 	var commands = [
@@ -54,4 +56,48 @@ gulp.task('deploy-test', ['require-js-clean'], function(){
 	return gulp.src(['./build/web/js/*'])
 		.pipe(uglify())
 		.pipe(gulp.dest('build/web/js'));
+});
+
+gulp.task('build-compress', ['require-js-clean'], function(){
+
+    var src = ['./build/**', './build/.*'];
+
+    return gulp.src(src)
+        .pipe(tar('build.tar'))
+        .pipe(gzip())
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('upload', ['build-compress'], function(){
+
+	var scpLogin = 'scp build.tar.gz sebastianjoesicecream.com@s191172.gridserver.com'
+    var uploadDir = '~/domains/flavors.sebastianjoesicecream.com/tmp/';
+
+	var sshLogin = 'ssh sebastianjoesicecream.com@s191172.gridserver.com';
+	var scpCommand = scpLogin + ':' + uploadDir + 'build.tar.gz';
+
+    var sshCommands = [
+        'cd ' + uploadDir,
+        'tar -xvzf build.tar.gz',
+    	'rm -rf ../html',
+    	'mv -f build/web/ ../html',
+    //     'rm -rf ../node-services',
+    //     'mv -f build/node-services/ ../',
+    //     'mv -f build/app.js ../',
+    //     'mv -f build/package.json ../',
+    //     'npm install --production',
+    //     'stop node-simple-http-daemon',
+    //     'start node-simple-http-daemon',
+    //     'stop node-simple-https-daemon',
+    //     'start node-simple-https-daemon',
+    	'rm -rf build build.tar.gz'
+    ].join(';');
+
+    var commands = [
+        scpCommand,
+		sshLogin + ' "' + sshCommands + '"'
+    ].join(';');
+
+    return gulp.src('')
+        .pipe(exec(commands, { silent: false }));
 });
--- web/admin/add-flavor.php
+++ web/admin/add-flavor.php
@@ -5,10 +5,8 @@ $store = $_POST['store'];
 $str_data = file_get_contents("../json/" . $store . ".json");
 $data = json_decode($str_data,true);
 
-
-
 $data["flavorIds"][] = (int)$_POST['flavorId'];
- 
+
 $fh = fopen("../json/" . $store . ".json", 'w')
       or die("Error opening output file");
 fwrite($fh, json_encode($data, JSON_UNESCAPED_UNICODE));
